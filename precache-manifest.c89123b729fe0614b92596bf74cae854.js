a839340caa0786481c75c988b02da1757bdc88fc
Suddha Brata De <walafashion716@gmail.com>
2020-10-04 11:30:18 +0530

    Updates

---
+++ precache-manifest.c89123b729fe0614b92596bf74cae854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700afbf76e82ceb7ce4aefd5b640215a",
+    "url": "/Cubecheap-todo-app/index.html"
+  },
+  {
+    "revision": "c4f8959178a5f9348dba",
+    "url": "/Cubecheap-todo-app/static/css/main.1ff72d68.chunk.css"
+  },
+  {
+    "revision": "9c3856a34353fe47b4f8",
+    "url": "/Cubecheap-todo-app/static/js/2.c7758592.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cubecheap-todo-app/static/js/2.c7758592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f8959178a5f9348dba",
+    "url": "/Cubecheap-todo-app/static/js/main.931046f8.chunk.js"
+  },
+  {
+    "revision": "d5694d7534c00b7c3336",
+    "url": "/Cubecheap-todo-app/static/js/runtime-main.966c79bc.js"
+  }
+]);
